333ecc690ca51aa0adcac9a301ec3f022aa24da1
deyanuz <103450547+deyanuz@users.noreply.github.com>
2024-04-01 07:21:22 +0600

    registration system

---
+++ app/Http/Controllers/RegisterController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+    public function index(){
+        return view("auth.register");
+    }
+
+    public function registerUser(Request $request)
+    {
+        $user = new User();
+
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+
+        $user->save();
+
+        return back()->with('success', 'Register successfully');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\CheckoutController;
+use App\Http\Controllers\ForgotController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\ResetController;
+use App\Http\Controllers\ShopController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+route::get('/home',[HomeController::class,'index'])->name('frontend.home');
+route::get('/shop',[ShopController::class,'index'])->name('frontend.shop');
+route::get('/cart',[CartController::class,'index'])->name('frontend.cart');
+route::get('/checkout',[CheckoutController::class,'index'])->name('frontend.checkout');
+route::get('/login',[LoginController::class,'index'])->name('auth.login');
+route::get('/register',[RegisterController::class,'index'])->name('auth.register');
+route::get('/forgot',[ForgotController::class,'index'])->name('auth.forgot');
+route::get('/reset',[ResetController::class,'index'])->name('auth.reset');
+
+route::post('/register',[RegisterController::class,'registerUser'])->name('auth.register');
