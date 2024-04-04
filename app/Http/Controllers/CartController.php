8ce44056dd9e89d44fa6d4e300a24d80159eaaff
deyanuz <103450547+deyanuz@users.noreply.github.com>
2024-04-04 22:21:30 +0600

    cart update

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -24,7 +24,7 @@ class CartController extends Controller
         $product = Product::where("id", $id)->first();
         if ($product->quantity > $qty) {
             Cart::add($product->id, $product->name, 1, $product->regular_price)->associate('App\Models\Product');
-            return redirect()->route('frontend.cart')->with('success', 'Item added successfully');
+            return redirect()->route('frontend.cart');
         }
         return redirect()->route('frontend.cart')->with('error', 'Maximum limit reached');
     }
